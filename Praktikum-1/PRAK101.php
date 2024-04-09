c75acc335891dac61eec57bb5d12823ab023d5f9
ZakiFir <114892217+ZakiFir@users.noreply.github.com>
2024-04-09 12:38:16 +0800

    Add files via upload

---
+++ Praktikum-1/PRAK101.php
@@ -0,0 +1,8 @@
+<?php 
+// Menampilkan atau mencetak String 
+    $nama = "Mohammad Zaki Firmansah";
+    $nim = "2210817110006";
+    echo "Hello World in PHP<br>"; 
+    echo "Nama Praktikan : $nama<br>"; 
+    echo "NIM : $nim"; 
+?>
